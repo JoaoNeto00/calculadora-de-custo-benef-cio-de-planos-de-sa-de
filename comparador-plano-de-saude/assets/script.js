e83aa95d59506cd465a80d678437a55ebe09f62a
JoaoNeto00 <joao.neto1883@gmail.com>
2023-11-17 14:04:43 -0300

    alteração formula

--- comparador-plano-de-saude/assets/script.js
+++ comparador-plano-de-saude/assets/script.js
@@ -44,8 +44,8 @@ function calculoOperadoraA() {
    const imc = Number(calculoIMC());
 
    const calcBasico = 100 + (idade * 10) * (imc / 10);   // Plano básico: 100 + (idade * 10 * (IMC / 10)).
-	const calcStandard = 150 + (idade * 15) * (imc / 10); // Plano standard: (150 + (idade * 15)) * (IMC / 10).
-   const calcPremium = 200 - (imc * 10) + (idade * 20) * (imc / 10);// Plano premium: (200 - (IMC * 10) + (idade * 20)) * (IMC / 10).
+	const calcStandard = (150 + (idade * 15)) * (imc / 10); // Plano standard: (150 + (idade * 15)) * (IMC / 10).
+   const calcPremium = (200 - (imc * 10) + (idade * 20)) * (imc / 10);// Plano premium: (200 - (IMC * 10) + (idade * 20)) * (IMC / 10).
    
    const planoBasico = calcBasico;
    const planoStandard = calcStandard;
@@ -61,7 +61,7 @@ function calculoOperadoraB() {
    const ftComorbidade = fatorComorbidade(imc);
 
    const calcBasico = 100 + (ftComorbidade * 10) * (imc / 10); //Plano básico: 100 + (fator de comorbidade * 10 * (IMC / 10)).
-   const calcStandard = (150 + ftComorbidade * 15) * (imc / 10);//Plano standard: (150 + (fator de comorbidade * 15)) * (IMC / 10).
+   const calcStandard = (150 + (ftComorbidade * 15)) * (imc / 10);//Plano standard: (150 + (fator de comorbidade * 15)) * (IMC / 10).
    const calcPremium = (200 - (imc * 10) + (ftComorbidade * 20) * (imc / 10)); //Plano premium: (200 - (IMC * 10) + (fator de comorbidade * 20)) * (IMC / 10).
 
    const planoBasico = calcBasico;
